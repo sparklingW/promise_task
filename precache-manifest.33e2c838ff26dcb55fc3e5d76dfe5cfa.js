749821d219e087f0d5e19ca71ce99b1441656938
ilya-velychko <ilya.velychko@gotoinc.co>
2020-06-03 14:02:10 +0300

    Updates

---
+++ precache-manifest.33e2c838ff26dcb55fc3e5d76dfe5cfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30acfdf79aa21af0332b03558306d087",
+    "url": "/promise_task/index.html"
+  },
+  {
+    "revision": "242b9f6c7e2896919997",
+    "url": "/promise_task/static/css/main.e1bf9926.chunk.css"
+  },
+  {
+    "revision": "d249f54dac5f64ca4067",
+    "url": "/promise_task/static/js/2.eba2d47a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/promise_task/static/js/2.eba2d47a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "242b9f6c7e2896919997",
+    "url": "/promise_task/static/js/main.8203d265.chunk.js"
+  },
+  {
+    "revision": "babc5c4b23094305d11d",
+    "url": "/promise_task/static/js/runtime-main.80751b24.js"
+  }
+]);
